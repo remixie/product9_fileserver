6a1fa3863e00c32e0250ba9e6587be4f11ded838
Francisco Salinas <remixielive@live.com>
2022-10-22 16:57:29 -0400

    Remove reloading.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,61 @@
 <script setup lang="ts">
-import Filelist from "./components/Filelist.vue";
+import { reactive } from "vue";
+import axios from "redaxios";
+import { ref } from "vue";
+
+let fileForm = ref()
+
+let submitFile = async () => {
+  let formData = new FormData(fileForm.value);
+  await axios({ method: 'post', url: '/fileupload', data: formData,  });
+  fetchData();
+  fileForm.value.reset()
+}
+
+let list = reactive({ data: [] });
+let fetchData = async () => {
+  const response = await axios.get("/filelist");
+  list.data = response.data;
+};
+let view = (filename: string) => {
+  window.location.href = "/file/" + filename;
+};
+let del = async (filename: string) => {
+  await axios.delete("/file/" + filename);
+  fetchData();
+};
+fetchData();
 </script>
 
 <template>
-  <form action="/fileupload" enctype="multipart/form-data" method="POST">
-    <input type="file" name="binary" multiple />
-    <input type="submit" value="Upload dataset(s)" />
+  <form
+    ref="fileForm"
+    style="display: inline-block"
+  >
+    <input
+      type="file"
+      name="file"
+    >
   </form>
-  <suspense>
-    <filelist />
-  </suspense>
+  <input
+    type="button"
+    style="display: inline-block"
+    value="Upload dataset"
+    @click="submitFile()"
+  >
+  <div>
+    <h2>Uploaded Dataset(s)</h2>
+  </div>
+  <div
+    v-for="l in list.data"
+    :key="l"
+  >
+    {{ l }}
+    <button @click="view(l)">
+      View
+    </button>
+    <button @click="del(l)">
+      Delete
+    </button>
+  </div>
 </template>
