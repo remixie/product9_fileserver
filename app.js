f54189e7e6723ecd5d47719c4b2b8793d5ab57b5
Francisco Salinas <remixielive@live.com>
2023-01-15 17:02:21 -0500

    Log iteration number.

--- app.js
+++ app.js
@@ -276,6 +276,7 @@ app.get("/detect-fields/:filename", async (req, res) => {
       }
       if (i === 10) console.log("Couldn't parse json after 1000 iterations");
 
+      console.log(i);
       console.log(data[0]);
 
       for (const property in data[0]) {
